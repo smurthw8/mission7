e70f3d30b67cd71ca1c55668de85abd107788c8e
smurthw8 <kirsten.smurthwaite@gmail.com>
2022-03-09 15:36:54 -0700

    part 2 videos

--- BookBuyer/Models/IBookBuyerRepositoy.cs
+++ BookBuyer/Models/IBookBuyerRepositoy.cs
@@ -6,6 +6,10 @@ namespace BookBuyer.Models
     public interface IBookBuyerRepository
     {
         IQueryable<Books> Books { get; }
+
+        public void SaveBook(Books b);
+        public void CreateBook(Books b);
+        public void DeleteBook(Books b);
     }
 
 }
